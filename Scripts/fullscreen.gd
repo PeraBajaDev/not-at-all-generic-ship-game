727852ad644d429c78d32eb662ef729a84c4ae78
Benjamín Peralta <benjaminp158@gmail.com>
2024-09-20 13:45:28 -0300

    Corrigiendo error faltaba variable

--- Scripts/fullscreen.gd
+++ Scripts/fullscreen.gd
@@ -1,6 +1,8 @@
 extends Node
 
-func _process(delta: float) -> void:
+var window = get_window()
+
+func _process(_delta: float) -> void:
 	if Input.is_key_pressed(KEY_F11):
 		if window.mode != Window.Mode.MODE_EXCLUSIVE_FULLSCREEN:
 			window.set_mode(Window.MODE_EXCLUSIVE_FULLSCREEN)
