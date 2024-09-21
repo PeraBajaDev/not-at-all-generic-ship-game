4d0cf2700ec374313114b7842a735423546bb7ed
Benjamín Peralta <benjaminp158@gmail.com>
2024-09-21 09:27:09 -0300

    Cambiado ruta de la escena

--- Scripts/Power Ups/bullet_spread_power_up.gd
+++ Scripts/Power Ups/bullet_spread_power_up.gd
@@ -4,4 +4,4 @@ extends PowerUp
 func apply(body: Node2D):
 	var canon: BlastCanon = body.get_node('BlastCanon')
 	if canon:
-		canon.bullet_scene = preload('res://Scenes/Shoot System/player_bullet_spreader.tscn')
+		canon.bullet_scene = preload('res://Scenes/Power Ups/player_bullet_spreader.tscn')
