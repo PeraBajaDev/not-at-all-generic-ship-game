f08c67eb497d76762eb1c819c4dc2a77a57626fb
Benjamín Peralta <benjaminp158@gmail.com>
2024-09-22 19:21:29 -0300

    Corregido bug de la barra de vida: Se desplazaba al reiniciar la escena

--- Scripts/Health System/health_bar.gd
+++ Scripts/Health System/health_bar.gd
@@ -2,10 +2,9 @@ extends TextureRect
 
 @export var player: PlayerStats
 var tween
-	
+
 func _process(_delta: float) -> void:
 	tween = get_tree().create_tween()
 	tween.parallel().tween_property(self, 'size', Vector2(player.maxHealth * 10, size.y), 0.5)
 	tween.parallel().tween_property($CurrentHealth, 'size', Vector2(player.health * 10, size.y), 0.5)
-	
-	print($CurrentHealth.size)
+	$CurrentHealth.position = Vector2.ZERO
