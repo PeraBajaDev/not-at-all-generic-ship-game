84348d9f8c464d15988181bce8a5778bea9ef4ac
Benjamín Peralta <benjaminp158@gmail.com>
2024-10-22 11:43:31 -0300

    Refactorizado creado un atributo bullet

--- Scripts/Shoot System/blast_canon.gd
+++ Scripts/Shoot System/blast_canon.gd
@@ -4,12 +4,12 @@ extends Node2D
 @export var bullet_scene: PackedScene
 @export var shooterNode: Node
 @export var damage: int
-
+var bullet: Bullet
 func _ready() -> void:
 	shooterNode.shoot.connect(on_shoot)
 
 func on_shoot():
-	var bullet: Node = bullet_scene.instantiate()
+	bullet = bullet_scene.instantiate()
 	bullet.damage = damage
 	bullet.position = self.global_position
 	get_parent().add_sibling(bullet)
