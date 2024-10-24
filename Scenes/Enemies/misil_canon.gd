2cc4077c56c3b1537b245cb1a6c75413f82f46e9
Benjamín Peralta <benjaminp158@gmail.com>
2024-10-24 19:44:20 -0300

    Refactorizado dependencia acoplada

--- Scenes/Enemies/misil_canon.gd
+++ Scenes/Enemies/misil_canon.gd
@@ -1,6 +1,8 @@
 extends BlastCanon
 
+@export var misilHint: Node2D
+
 func on_shoot():
 	super.on_shoot()
 	if bullet.has_method('go_to_target'):
-		bullet.go_to_target($"../MisilHint".global_position)
+		bullet.go_to_target(misilHint.global_position)
