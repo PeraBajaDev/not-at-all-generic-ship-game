d72dbadec915df31a35037244381308c502f5746
Benjamín Peralta <benjaminp158@gmail.com>
2024-10-24 19:35:54 -0300

    Añadido la capacidad de seguir constantemente al jugador con los misiles

--- direction_hint.gd
+++ direction_hint.gd
@@ -1,15 +1,18 @@
 extends Sprite2D
 
 @export var playerStats: PlayerStats
+@export var waitTimeToLocalizePlayer: float = 2
+@export var is_always_seeking: bool = false
 var timer: SceneTreeTimer
 var smoothTime: float = 0
 signal playerLocalized(position: Vector2)
+
 func _ready():
-	timer = get_tree().create_timer(1)
+	timer = get_tree().create_timer(waitTimeToLocalizePlayer)
 
 func _process(delta: float) -> void:
 	smoothTime += delta * 2
-	if timer.time_left <= 0:
+	if timer.time_left <= 0 and not is_always_seeking:
 		playerLocalized.emit(position) 
 		return
 		
