d270107be1f3ff1606d4f03e0695a1bb3686b4ec
Benjamín Peralta <benjaminp158@gmail.com>
2024-10-26 13:28:52 -0300

    Añadido animación de aparición del jefe

--- boss_laser.gd
+++ boss_laser.gd
@@ -2,5 +2,5 @@ extends Bullet
 
 func _ready() -> void:
 	var tween = create_tween()
-	tween.tween_property(self, 'rotation_degrees', -90, 1).as_relative()
+	tween.tween_property(self, 'rotation_degrees', -90, 0.5).as_relative().set_ease(Tween.EASE_IN).set_delay(0.5)
 	
