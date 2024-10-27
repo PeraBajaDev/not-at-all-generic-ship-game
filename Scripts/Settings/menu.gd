918c59feff9b762ea96b55ac247fc33cb9006f0d
Benjamín Peralta <benjaminp158@gmail.com>
2024-10-27 13:00:00 -0300

    Corregido el botón start no comenzaba la partida

--- Scripts/Settings/menu.gd
+++ Scripts/Settings/menu.gd
@@ -1,8 +1,8 @@
 extends Control
 	
-
+@export var level: PackedScene
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	get_tree().change_scene_to_packed(level)
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
