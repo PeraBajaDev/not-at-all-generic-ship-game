dcaedc4d45dbddd4774ce1ce01534cc7b6f01a98
Benjamín Peralta <benjaminp158@gmail.com>
2024-10-31 17:43:44 -0300

    Arreglado error de botones que no funcionaban correctamente al cambiar de escena

--- Scripts/Boss/change_to_win_scene.gd
+++ Scripts/Boss/change_to_win_scene.gd
@@ -1,10 +1,9 @@
 extends Node
 
-@export var winScene: PackedScene
 func _ready() -> void:
 	GameState.boss_defeated.connect(change_to_win_scene)
 
 func change_to_win_scene() -> void:
 	print('jefe eliminado')
 	await get_tree().create_timer(3).timeout
-	get_tree().change_scene_to_packed(winScene)
+	get_tree().change_scene_to_file("res://Scenes/Wave System/win_screen.tscn")
