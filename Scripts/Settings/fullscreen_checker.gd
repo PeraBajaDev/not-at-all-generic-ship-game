4e84d453f9cc4a1149914ca4b9a6b1ba83b0245b
Benjamín Peralta <benjaminp158@gmail.com>
2024-11-02 10:35:33 -0300

    Arreglado botón de fullscreen no se mostraba correctamente

--- Scripts/Settings/fullscreen_checker.gd
+++ Scripts/Settings/fullscreen_checker.gd
@@ -3,13 +3,11 @@ extends CheckBox
 @onready var window = get_window()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	button_pressed = window.mode == Window.MODE_EXCLUSIVE_FULLSCREEN
 	toggled.connect(on_button_pressed)
 
 func on_button_pressed(checked: bool): 
 	if checked:
 		window.mode = Window.MODE_EXCLUSIVE_FULLSCREEN
 	else:
-		window.mode = Window.MODE_WINDOWED 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+		window.mode = Window.MODE_WINDOWED
