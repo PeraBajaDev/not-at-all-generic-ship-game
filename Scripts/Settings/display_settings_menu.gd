0775e93fe6e5f710dc8fb65495754a028893310e
Benjamín Peralta <benjaminp158@gmail.com>
2024-12-11 09:40:49 -0300

    Agregado un botón de pausa para los controles virtuales

--- Scripts/Settings/display_settings_menu.gd
+++ Scripts/Settings/display_settings_menu.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 func on_press_close_button():
 	self.visible = false
 	get_tree().paused = false
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("open_menu"):
 		self.visible = !visible
 		get_tree().paused = !get_tree().paused
