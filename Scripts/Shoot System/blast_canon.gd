b9bcf9381ae77edb08dfcb9d2c4c729c97e75e1c
Benjamín Peralta <benjaminp158@gmail.com>
2025-03-09 23:45:58 -0300

    Revert "Testeando habilidades temporales"
    This reverts commit 08fe1d57fde129d43c466cd6ee47c7e043c6d47a.

--- Scripts/Shoot System/blast_canon.gd
+++ Scripts/Shoot System/blast_canon.gd
@@ -1,23 +1,15 @@
 class_name BlastCanon
 extends Node2D
 
-@export var normal_bullet_scene: PackedScene
+@export var bullet_scene: PackedScene
 @export var shooterNode: Node
 @export var damage: int
 var bullet: Bullet
-var bullet_scene: PackedScene = normal_bullet_scene
 func _ready() -> void:
 	shooterNode.shoot.connect(on_shoot)
+
 func on_shoot():
 	bullet = bullet_scene.instantiate()
 	bullet.damage = damage
 	bullet.position = self.global_position
 	get_parent().add_sibling(bullet)
-
-func change_on_power_up(new_bullet: PackedScene, seconds: int):
-	bullet_scene = new_bullet
-	await get_tree().create_timer(seconds)
-	change_to_normal_bullet()
-
-func change_to_normal_bullet():
-	bullet_scene = normal_bullet_scene
