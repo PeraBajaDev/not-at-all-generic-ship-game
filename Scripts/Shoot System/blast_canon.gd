8a686af90851e005f6dcd5f4605d75630b1d04fc
Benjamín Peralta <benjaminp158@gmail.com>
2025-03-15 15:38:07 -0300

    Añadido poder de lanzar misiles

--- Scripts/Shoot System/blast_canon.gd
+++ Scripts/Shoot System/blast_canon.gd
@@ -1,15 +1,6 @@
 class_name BlastCanon
-extends Node2D
-
-@export var bullet_scene: PackedScene
-@export var shooterNode: Node
-@export var damage: int
-var bullet: Bullet
-func _ready() -> void:
-	shooterNode.shoot.connect(on_shoot)
+extends BaseCanon
 
 func on_shoot():
-	bullet = bullet_scene.instantiate()
-	bullet.damage = damage
-	bullet.position = self.global_position
+	super.on_shoot()
 	get_parent().add_sibling(bullet)
---
+++ bullet_strategy.gd
@@ -0,0 +1,8 @@
+class_name BulletStrategy
+extends Node
+
+@warning_ignore("unused_signal")
+signal timeout
+
+func apply_upgrade(bullet: Bullet):
+	printerr("Usando clase base de bullet strategy")
